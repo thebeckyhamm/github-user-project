b6be4193e9d97444a52353cbdd4a07d06a85e63d
Becky Hamm <becky@metricslice.com>
2019-10-30 13:32:48 -0700

    Task: Get search working on button click, instead of keystroke

--- src/components/UserSearch.js
+++ src/components/UserSearch.js
@@ -3,26 +3,41 @@ import { getUsers } from "../utils/api";
 
 export default function UserSearch() {
   const [results, setResults] = React.useState([]);
-  const [loading, setLoading] = React.useState(true);
-  const [queryString, setQueryString] = React.useState("thebeckyhamm");
+  const [search, setSearch] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const [queryString, setQueryString] = React.useState("");
 
   React.useEffect(() => {
-    setLoading(true);
-    getUsers(queryString).then((data) => {
-      setResults(data.items);
-      setLoading(false);
-    });
-  }, [queryString]);
+    if (!!search) {
+      setLoading(true);
+      getUsers(search).then((data) => {
+        setResults(data.items);
+        setLoading(false);
+      });
+    }
+  }, [search]);
 
   if (loading) {
     return <h3>Loading</h3>;
   }
 
   return (
-    <ul>
-      {results.map(({ login, id, avatar_url, url }) => {
-        return <li key={id}>{login}</li>;
-      })}
-    </ul>
+    <React.Fragment>
+      <input
+        type='text'
+        placeholder='Search for a user'
+        value={queryString}
+        onChange={(e) => setQueryString(e.target.value)}
+      />
+      <button onClick={() => setSearch(queryString)}>Search</button>
+      {loading && <h3>Loading</h3>}
+      {results && (
+        <ul>
+          {results.map(({ login, id, avatar_url, url }) => {
+            return <li key={id}>{login}</li>;
+          })}
+        </ul>
+      )}
+    </React.Fragment>
   );
 }
