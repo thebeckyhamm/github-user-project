4882ae23a558ab8fb7a287da970e899791f2013f
Becky Hamm <becky@metricslice.com>
2019-11-01 10:09:49 -0700

    Task: Add basic support for routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.scss";
 import Users from "./components/Users/Users";
+import Profile from "./components/Profile/Profile";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -9,7 +11,13 @@ function App() {
         <h1 className='header__text'>Github Users</h1>
       </header>
       <div className='wrapper'>
-        <Users />
+        <Router>
+          <Switch>
+            <Route path='/search/users' component={Users} />
+            <Route path='/user/profile' component={Profile} />
+            <Route component={Users} />
+          </Switch>
+        </Router>
       </div>
     </div>
   );
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Profile.scss";
+
+export default function Profile() {
+  return <h1>test</h1>;
+}
