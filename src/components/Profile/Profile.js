7fa4bcfea8b538bf138a85e1f07756d2b3fd9324
Becky Hamm <becky@metricslice.com>
2019-11-01 12:37:11 -0700

    Task: Get getUser api working and displaying

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,41 @@
 import React from "react";
 import "./Profile.scss";
 import { useParams } from "react-router-dom";
+import { getUser } from "../../utils/api";
 
 export default function Profile() {
   const { login } = useParams();
-  return <h1>{login}</h1>;
+  const [loading, setLoading] = React.useState(true);
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    setLoading(true);
+    getUser(login).then((data) => {
+      console.log(data);
+      setUser(data);
+      setLoading(false);
+      console.log(user);
+    });
+  }, [login]);
+
+  if (loading) {
+    return <p>Loading</p>;
+  }
+
+  return (
+    <div className='flex'>
+      <img
+        className='profile__img p--r-1'
+        src={user.avatar_url}
+        alt={`Profile avatar for ${user.login}`}
+      />
+      <h1>{user.login}</h1>
+      {user.bio && <p>{user.bio}</p>}
+      {user.blog && (
+        <p>
+          <strong>Website:</strong> {user.blog}
+        </p>
+      )}
+    </div>
+  );
 }
