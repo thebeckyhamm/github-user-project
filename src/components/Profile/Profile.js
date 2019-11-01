888cd24c81df4ee03f143bfaffbbbaa1de77be7f
Becky Hamm <becky@metricslice.com>
2019-11-01 13:10:41 -0700

    Task: Set up followers, following, and repos

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,41 +1,108 @@
 import React from "react";
 import "./Profile.scss";
 import { useParams } from "react-router-dom";
-import { getUser } from "../../utils/api";
+import { getUser, getFollowers, getFollowing, getRepos } from "../../utils/api";
 
 export default function Profile() {
   const { login } = useParams();
   const [loading, setLoading] = React.useState(true);
   const [user, setUser] = React.useState(null);
+  const [followers, setFollowers] = React.useState([]);
+  const [following, setFollowing] = React.useState([]);
+  const [repos, setRepos] = React.useState([]);
+  const [error, setError] = React.useState(null);
 
   React.useEffect(() => {
     setLoading(true);
-    getUser(login).then((data) => {
-      console.log(data);
-      setUser(data);
-      setLoading(false);
-      console.log(user);
-    });
+    const userData = getUser(login);
+    const followerData = getFollowers(login);
+    const followingData = getFollowing(login);
+    const repoData = getRepos(login);
+
+    Promise.all([userData, followerData, followingData, repoData])
+      .then((data) => {
+        setUser(data[0]);
+        setFollowers(data[1]);
+        setFollowing(data[2]);
+        setRepos(data[3]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
   }, [login]);
 
   if (loading) {
     return <p>Loading</p>;
   }
 
-  return (
-    <div className='flex'>
-      <img
-        className='profile__img p--r-1'
-        src={user.avatar_url}
-        alt={`Profile avatar for ${user.login}`}
-      />
-      <h1>{user.login}</h1>
-      {user.bio && <p>{user.bio}</p>}
-      {user.blog && (
+  if (error) {
+    return (
+      <React.Fragment>
         <p>
-          <strong>Website:</strong> {user.blog}
+          There has been an error. Please see the message below for details:
         </p>
-      )}
+        <p>"{error}"</p>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <div>
+      <div className='flex'>
+        <img
+          className='profile__img p--r-1'
+          src={user.avatar_url}
+          alt={`Profile avatar for ${user.login}`}
+        />
+        <div>
+          <h1>{user.login}</h1>
+          {user.bio && <p>{user.bio}</p>}
+          {user.blog && (
+            <p>
+              <strong>Website:</strong> {user.blog}
+            </p>
+          )}
+        </div>
+      </div>
+      <div className='flex'>
+        <div className='flex--1'>
+          <h2>Followers</h2>
+          {followers.length > 0 ? (
+            <ul>
+              {followers.map((follower) => {
+                return <li key={follower.id}>{follower.login}</li>;
+              })}
+            </ul>
+          ) : (
+            <p>This user has no followers.</p>
+          )}
+        </div>
+        <div className='flex--1'>
+          <h2>Following</h2>
+          {following.length > 0 ? (
+            <ul>
+              {following.map((followee) => {
+                return <li key={followee.id}>{followee.login}</li>;
+              })}
+            </ul>
+          ) : (
+            <p>This user is not following anyone.</p>
+          )}
+        </div>
+      </div>
+      <div>
+        <h2>Repos</h2>
+        {repos.length > 0 ? (
+          <ul>
+            {repos.map((repo) => {
+              return <li key={repo.id}>{repo.full_name}</li>;
+            })}
+          </ul>
+        ) : (
+          <p>This user has no repos.</p>
+        )}
+      </div>
     </div>
   );
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -9,3 +9,21 @@ export function getUser(username = "thebeckyhamm") {
     res.json()
   );
 }
+
+export function getFollowers(username) {
+  return fetch(
+    `https://api.github.com/users/${username}/followers?per_page=5`
+  ).then((res) => res.json());
+}
+
+export function getFollowing(username) {
+  return fetch(
+    `https://api.github.com/users/${username}/following?per_page=5`
+  ).then((res) => res.json());
+}
+
+export function getRepos(username) {
+  return fetch(
+    `https://api.github.com/users/${username}/repos?per_page=5`
+  ).then((res) => res.json());
+}
